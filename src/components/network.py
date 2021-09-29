0bceef02893cb230906026d43f78b8bf5acea527
Sverre <43172180+Sverrevg@users.noreply.github.com>
2021-09-29 16:48:00 +0200

    Working on backprop function

--- src/components/network.py
+++ src/components/network.py
@@ -17,14 +17,6 @@ def calc_layer_output(neurons, x):
     return output
 
 
-# def backpropagation(neuron, loss):
-#     # Application of the chain rule to find derivative of the loss function with respect to weights
-#     d_weights = np.dot(neuron.signal, (2 * loss * sigmoid_derivative(loss)))
-#
-#     neuron.update_weights(round(d_weights, 4))
-#     print(d_weights)
-
-
 class Network:
     def __init__(self, input_dim):
         # Create input layer based on input dimensions:
@@ -57,6 +49,7 @@ class Network:
 
             # Then, calculate backpropagation for each layer:
             # Output layer first, using the prediction made:
+            self.backpropagation(self.loss)
             print(f"Iteration {iteration}: \n loss: {self.loss}")
 
             # Perform backprop here to change weights and biases.
@@ -72,5 +65,33 @@ class Network:
 
     def calc_total_loss(self, y, output):
         # Use square function
-        loss = (y - output) * (y - output)
+        loss = np.square(y - output)
         self.loss += loss
+
+    def backpropagation(self, loss):
+        # Start with output layer:
+        d_weights = np.dot(self.output_layer.signal, (2 * loss * sigmoid_derivative(loss)))
+        self.output_layer.update_weights(d_weights)
+
+        # Get all weights for input layer and store in array. Index corresponds to index in node array:
+        node_weights = [[]]
+        for value_pair in self.output_layer.value_pairs:
+            # Create array as there can be multiple weights connected to previous nodes:
+            weight = [value_pair.get_weight()]
+            node_weights.append(weight)
+
+        # Then hidden layers:
+        for layer in self.hidden_layers:
+            # Get all nodes in layer:
+            for i, neuron in enumerate(layer.neurons):
+                # Then sum up all weights associated with this node:
+                summed_weights = []
+                for weight in node_weights[i]:
+                    summed_weights += weight
+
+                d_weights = np.dot(neuron.signal, (2 * loss * sigmoid_derivative(summed_weights)))
+                neuron.update_weights(d_weights)
+
+            # Next, get updated weights for next layer:
+            node_weights = [[]]  # reset weight array.
+            
