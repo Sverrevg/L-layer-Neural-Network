0dd211ff900258b363e80d5b53a71741c35f94f4
Sverre <43172180+Sverrevg@users.noreply.github.com>
2021-11-23 15:04:30 +0100

    Update cat_train.py

--- src/poc/cat_train.py
+++ src/poc/cat_train.py
@@ -1,6 +1,7 @@
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
+from imblearn.over_sampling import SMOTE
 
 from neural_net import NeuralNetwork
 
@@ -22,45 +23,45 @@ def load_data():
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
 
-train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+X_train_origin, y_train, X_test_origin, y_test, classes = load_data()
 
 # Example of a picture
 index = 88
-plt.imshow(train_x_orig[index])
+plt.imshow(X_train_origin[index])
 plt.show()
-print("y = " + str(train_y[0, index]) + ". It's a " + classes[train_y[0, index]].decode("utf-8") + " picture.\n")
+print("y = " + str(y_train[0, index]) + ". It's a " + classes[y_train[0, index]].decode("utf-8") + " picture.\n")
 
-# Explore your dataset
-m_train = train_x_orig.shape[0]
-num_px = train_x_orig.shape[1]
-m_test = test_x_orig.shape[0]
+# Explore your dataset:
+image_size = X_train_origin.shape[1]
+m_train = X_train_origin.shape[0]
+m_test = X_test_origin.shape[0]
 
 print("Number of training examples: " + str(m_train))
 print("Number of testing examples: " + str(m_test))
-print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
-print("train_x_orig shape: " + str(train_x_orig.shape))
-print("train_y shape: " + str(train_y.shape))
-print("test_x_orig shape: " + str(test_x_orig.shape))
-print("test_y shape: " + str(test_y.shape) + "\n")
+print("Each image is of size: (" + str(image_size) + ", " + str(image_size) + ", 3)")
+print("train_x_orig shape: " + str(X_train_origin.shape))
+print("train_y shape: " + str(y_train.shape))
+print("test_x_orig shape: " + str(X_test_origin.shape))
+print("test_y shape: " + str(y_test.shape) + "\n")
 
-# Reshape the training and test examples
-train_x_flatten = train_x_orig.reshape(
-    train_x_orig.shape[0], -1).T  # The "-1" makes reshape flatten the remaining dimensions
-test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+# Reshape the training and test examples:
+# The "-1" makes reshape flatten the remaining dimensions:
+X_train_flatten = X_train_origin.reshape(X_train_origin.shape[0], -1).T
+X_test_flatten = X_test_origin.reshape(X_test_origin.shape[0], -1).T
 
-# Standardize data to have feature values between 0 and 1.
-train_x = train_x_flatten / 255.
-test_x = test_x_flatten / 255.
+# Standardize data to have feature values between 0 and 1:
+X_train = X_train_flatten / 255.
+X_test = X_test_flatten / 255.
 
-print("train_x's shape: " + str(train_x.shape))
-print("test_x's shape: " + str(test_x.shape) + "\n")
+print("X_train's shape: " + str(X_train.shape))
+print("X_test's shape: " + str(X_test.shape) + "\n")
 
 # Constants
-layers_dims = [12288, 15, 9, 5, 1]  # 4-layer model
+layers_dims = [12288, 15, 9, 5, 3, 1]  # 5-layer model
 
 nn = NeuralNetwork(layers_dims, learning_rate=0.005, num_iterations=1800, activation="sigmoid")
 
-nn.fit(train_x, train_y)
+nn.fit(X_train, y_train)
 
 # Save parameters for later use:
 nn.save_model()
@@ -72,4 +73,4 @@ ax.set(xlabel='Iterations (x10)', ylabel='Cost', title='Cost over iterations')
 plt.show()
 
 # Test model:
-pred_test = nn.test(test_x, test_y)
+pred_test = nn.test(X_test, y_test)
